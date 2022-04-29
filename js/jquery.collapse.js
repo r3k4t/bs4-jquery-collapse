caff1ffac27238d17befbb455d8cd15a9e46606a
Rahat Khan Tusar <gb229855@gmail.com>
2022-04-29 09:47:22 +0600

    bs4-jquery-collapse

---
+++ js/jquery.collapse.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('.btn-primary').click(function () {
+        $('.collapse').collapse('toggle')
+    })
+
+    $('.btn-success').click(function () {
+        $('.collapse').collapse('show')
+
+    })
+
+    $('.btn-warning').click(function () {
+        $('.collapse').collapse('hide')
+    })
+
+
+
+})
+
+
